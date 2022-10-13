b877d7f8790d04132403ad9b5436057fd7c64279
zarina86 <zara86tech@gmail.com>
2022-10-13 21:09:35 +0600

    devise files are modified

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,7 +92,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   
    # Mailjet mailer
-   config.action_mailer.delivery_method = :mailjet
+   config.action_mailer.delivery_method = :mailjet_api
    
-   config.action_mailer.default_url_options = { host: 'stormy-mountain-96906.herokuapp.com' }
+   config.action_mailer.default_url_options = { host: ENV['MAILER_URL'] }
 end
