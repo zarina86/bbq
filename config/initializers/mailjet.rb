156cd43385fbfc35a1e82052421342cda9913a97
zarina86 <zara86tech@gmail.com>
2022-10-15 16:36:59 +0600

    devise recoverable is added

--- config/initializers/mailjet.rb
+++ config/initializers/mailjet.rb
@@ -2,12 +2,11 @@
 Mailjet.configure do |config|
   config.api_key = Rails.application.credentials.dig(:mailjet, :api_key)
   config.secret_key = Rails.application.credentials.dig(:mailjet, :secret_key)
-  #config.default_from = Rails.application.credentials.dig(:mailjet, :sender)
+  config.default_from = Rails.application.credentials.dig(:mailjet, :sender)
   # Mailjet API v3.1 is at the moment limited to Send API.
   # We’ve not set the version to it directly since there is no other endpoint in that version.
   # We recommend you create a dedicated instance of the wrapper set with it to send your emails.
   # If you're only using the gem to send emails, then you can safely set it to this version.
   # Otherwise, you can remove the dedicated line into config/initializers/mailjet.rb.
   config.api_version = 'v3.1'
-  #config.end_point = 'https://api.mailjet.com'
 end
