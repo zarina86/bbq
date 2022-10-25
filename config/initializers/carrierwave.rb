1a925d4b5be50e3130bb58622a9614676a1d30e5
zarina86 <zara86tech@gmail.com>
2022-10-25 13:32:28 +0600

    next

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -10,14 +10,15 @@ if Rails.env.production?
     config.aws_authenticated_url_expiration = 60 * 60 * 24 * 7
 
     config.aws_credentials = {
-      access_key_id: "#{Rails.application.credentials.dig(:aws, :access_key_id)}"
-      secret_access_key: "#{Rails.application.credentials.dig(:aws, :secret_access_key)}"
-      region: "#{Rails.application.credentials.dig(:aws, :region)}"
+      access_key_id: <%= Rails.application.credentials.dig(:aws, :access_key_id) %>,
+      secret_access_key: <%= Rails.application.credentials.dig(:aws, :secret_access_key) %>,
+      region: <%= Rails.application.credentials.dig(:aws, :region) %>,
+    }
       
       #access_key_id:     ENV.fetch('AWS_ACCESS_KEY_ID'),
     #secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
     #region:            ENV.fetch('AWS_REGION'), # Required
     #stub_responses:    Rails.env.test? # Optional, avoid hitting S3 actual during tests
-  }   
+    
   end
 end
